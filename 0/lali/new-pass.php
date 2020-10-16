13fd9500b8436bcc35fb30aae16c673e3a0c42ec
Agung Dwicahyadi <adwicahyadi93@gmail.com>
2020-10-16 17:19:19 +0700

    ga jadi Auth lah mumet

--- 0/lali/new-pass.php
+++ 0/lali/new-pass.php
@@ -1,14 +1,13 @@
 <?php
 error_reporting(0);
 include "../../koneksi.php";
-if(isset($_POST["new_password"])){
-    $email 		= htmlspecialchars($_POST["email"]);
-    $password 	= md5($_POST["password"]);
-    $pass 		= htmlspecialchars($_POST["pass"]);
-    $query = mysqli_query($koneksi, "UPDATE superadmin SET password = '$password', pass = '$pass' WHERE email = '$email'");
+if(isset($_POST["new_pass"])){
+    $email 		= $_POST["email"];
+    $pass 		= md5($_POST["password"]);
+    $query 		= mysqli_query($koneksi, "UPDATE superadmin SET password = '$pass' WHERE email = '$email'");
     if($query){
-                mysqli_query($koneksi, "DELETE FROM reset_passwordword WHERE email = '$email'");
+                mysqli_query($koneksi, "DELETE FROM reset_password WHERE email = '$email'");
     }
-    echo "<script language = 'javascript'> alert ('Selamat password baru anda terupdate'); window.location='../login'</script> ";
+    echo "<script language = 'javascript'> alert ('Selamat Password baru anda terupdate'); window.location='../login'</script> ";
 }
 ?>
